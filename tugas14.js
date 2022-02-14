4f7f4c777e0628ae302ee42dc6e1461f3c4ff8e5
wan daffa abdilla <wandaffaadilla@gmail.com>
2022-02-14 23:11:08 +0800

    Tugas 14 JS
    Tugas ini kami kumpulkan untuk niomic

---
+++ tugas14.js
@@ -0,0 +1,13 @@
+function tugas14() {
+  var x = 37;
+  var y = 20;
+
+  return x / y;
+}
+console.log("ini adalah hasil pembagian :");
+console.log(tugas14());
+console.log("================================================");
+console.log("ini adalah hasil Math object method :");
+console.log(Math.round(tugas14()));
+console.log(Math.ceil(tugas14()));
+console.log(Math.floor(tugas14()));
